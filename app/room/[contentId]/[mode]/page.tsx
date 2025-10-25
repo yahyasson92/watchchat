f17242eb819c09fe711aced4d37b38b167fdeae2
yahyasson92 <73088376+yahyasson92@users.noreply.github.com>
2025-10-25 14:33:36 -0600

    Update page.tsx

--- app/room/[contentId]/[mode]/page.tsx
+++ app/room/[contentId]/[mode]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
-import { supabase } from '../../../lib/supabaseClient';
-import MessageInput from '../../components/MessageInput';
+import { supabase } from '../../../../lib/supabaseClient';
+import MessageInput from '../../../components/MessageInput';
 
 export default function Room({ params }: { params: { contentId: string; mode: 'global'|'followers' }}) {
   const { contentId, mode } = params;
