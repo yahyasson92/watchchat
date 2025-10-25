e7a4754cf1cebd14bdcbbac0238a8f03e9f25394
yahyasson92 <73088376+yahyasson92@users.noreply.github.com>
2025-10-25 14:36:56 -0600

    Update ContentPicker.tsx

--- app/components/ContentPicker.tsx
+++ app/components/ContentPicker.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '../../../../lib/supabaseClient';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
