1b6545992b4299b2ed113e61dcccd58ba8cad883
yahyasson92 <73088376+yahyasson92@users.noreply.github.com>
2025-10-25 13:59:01 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { supabase } from '@/lib/supabaseClient';
-import { supabase } from '../lib/supabaseClient';
 import ContentPicker from './components/ContentPicker';
 
 export default function Home() {
